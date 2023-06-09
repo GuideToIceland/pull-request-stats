7d42a47b47cc310bd289189b82de1fc3477f7d95
Sigurður Guðbrandsson <siggi@snjall.is>
2023-06-09 15:16:52 +0000

    Remove sponsorship check

--- src/interactors/checkSponsorship/index.js
+++ src/interactors/checkSponsorship/index.js
@@ -1,14 +1,7 @@
-const { fetchSponsorships } = require('../../fetchers');
-const getLogins = require('./getLogins');
-const isSponsoring = require('./isSponsoring');
-const isExternalSponsor = require('./isExternalSponsor');
-
-module.exports = async ({
+module.exports = ({
   octokit,
   org,
   repos,
 }) => {
-  const logins = getLogins({ org, repos });
-  const { user } = await fetchSponsorships({ octokit, logins });
-  return isSponsoring(user) || isExternalSponsor(logins);
+  return true;
 };
